eae3b3203a00030c199f4e2dcdbb65232a8a628b
Bogdan <babak.47031@gmail.com>
2012-07-20 21:54:26 +0300

    Updates: 1) Added forgotten files 2) Added library path with ckedior and funcybox

---
+++ library/ckeditor/config.js
@@ -0,0 +1,43 @@
+﻿/*
+Copyright (c) 2003-2011, CKSource - Frederico Knabben. All rights reserved.
+For licensing, see LICENSE.html or http://ckeditor.com/license
+*/
+
+CKEDITOR.editorConfig = function( config )
+{
+	// Define changes to default configuration here. For example:
+    config.language = "en";
+    config.extraPlugins = "tableresize,youtube";
+    config.removePlugins = "resize";
+    
+    config.skin = 'BootstrapCK-Skin';
+    config.filebrowserImageUploadUrl = "upload/ckeditor/";
+    
+    config.toolbar_admin =
+    [
+        { name: 'document',    items : [ 'Source','Preview','Print','Templates' ] },
+        { name: 'clipboard',   items : [ 'Cut','Copy','Paste','PasteText','PasteFromWord','-','Undo','Redo' ] },
+        { name: 'editing',     items : [ 'Replace','-','SelectAll' ] },
+        { name: 'basicstyles', items : [ 'Bold','Italic','Underline','Strike','Subscript','Superscript','-','RemoveFormat' ] },
+        '/',
+        { name: 'paragraph',   items : [ 'NumberedList','BulletedList','Outdent','Indent','Blockquote','CreateDiv','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','BidiLtr','BidiRtl' ] },
+        { name: 'links',       items : [ 'Link','Unlink','Anchor' ] },
+        { name: 'insert',      items : [ 'Image','Flash','Table','HorizontalRule','Smiley','SpecialChar' ] },
+        { name: 'tools',       items : [ 'Maximize', 'ShowBlocks' ] },
+        '/',
+        { name: 'styles',      items : [ 'Styles','Format','Font','FontSize','Youtube' ] },
+        { name: 'colors',      items : [ 'TextColor','BGColor' ] }
+    ];
+    
+    config.toolbar_basic =
+    [
+        { name: 'document',    items :['Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink','-','Undo','Redo','-','Smiley','-','FontSize','Blockquote'] },
+    ];
+    
+    config.smiley_images = ['aa.gif','ab.gif','ac.gif', 'ad.gif', 'ae.gif', 'af.gif', 'ag.gif', 'ah.gif', 'ai.gif', 'aj.gif', 'ak.gif', 'al.gif', 'am.gif', 'an.gif', 'ao.gif', 'ap.gif',
+        'aq.gif', 'ar.gif', 'as.gif', 'at.gif', 'au.gif', 'av.gif', 'aw.gif', 'ax.gif', 'ay.gif', 'az.gif', 'ba.gif', 'bc.gif', 'bd.gif', 'be.gif', 'bf.gif', 'bg.gif', 'bh.gif',
+        'bi.gif', 'bj.gif', 'bk.gif', 'bl.gif', 'bm.gif', 'bn.gif', 'bo.gif', 'bp.gif', 'bq.gif', 'br.gif', 'bs.gif', 'bt.gif', 'bu.gif', 'bv.gif', 'bw.gif', 'bx.gif', 'by.gif', 'bz.gif', 
+        'ca.gif', 'cb.gif', 'cc.gif', 'cd.gif', 'ce.gif', 'cf.gif', 'cg.gif', 'ch.gif', 'ci.gif', 'cj.gif', 'ck.gif', 'cl.gif', 'cm.gif', 'cn.gif', 'co.gif', 'cp.gif', 'cq.gif', 'cr.gif', 
+        'cs.gif', 'ct.gif', 'cu.gif', 'cv.gif', 'cw.gif', 'cx.gif', 'cy.gif', 'cz.gif', 'da.gif', 'db.gif', 'dc.gif' ];
+
+};
---
+++ web/controllers/Controller_ui.php
@@ -0,0 +1,34 @@
+<?php
+class Controller_ui extends Controller {
+    public function index() {
+        $this->val['value'] = 'test';
+        $this->template()->load('test_values.tpl')->show();
+    }
+
+    public function textfields() {
+        $this->val['text'] = 'Test value';
+        $this->template()->load('textfield.tpl')->show();
+    }
+
+    public function textarea() {
+        $this->template()->load('textarea.tpl')->show();
+    }
+
+    public function submit_and_button() {
+        $this->template()->load('submit.tpl')->show();
+    }
+
+    public function ckeditor() {
+        $this->template()->load('ckeditor.tpl')->show();
+    }
+
+    public function select() {
+        $this->val['data']['text_test1'] = array('{config::app_path}', 'test2');
+        $this->template()->load('select.tpl')->show();
+    }
+
+    public function radio() {
+        $this->val['data']['array'] = array('{config::app_path}', 'qwe');
+        $this->template()->load('radio.tpl')->show();
+    }
+}
