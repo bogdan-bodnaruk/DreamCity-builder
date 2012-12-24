82f5bedb5e6fda7d02e6a168f28bc4a3cfc88110
babak <babak.47031@gmail.com>
2012-12-24 23:36:58 +0200

    min: css min look like as completed.

--- cms/controllers/Controller_min.php
+++ cms/controllers/Controller_min.php
@@ -23,69 +23,83 @@ class Controller_min extends Controller {
         '#FF0000' =>  '#F00'
     );
 
-    public function __construct() {
-        parent::__construct();
-    }
-
-    public function index() {
-
-    }
+    public function index() {}
 
     public function css() {
         include_once(PATH.'.config/dependencies.php');
         $this->val('f');
-        $this->temp = PATH.$this->registry()->config['app_path'].'/theme/.temp';
 
-        $files = explode(';',$this->val['f']);
-        for($i=0;$i<count($files);$i++) {
-            if(!empty($files[$i])) {
-                $this->css .= file_get_contents(PATH.$D_css[$files[$i]]);
+        $this->temp = PATH.$this->registry()->config['app_path'].'/theme/.temp';
+        $bundle = true;
+        $filesDate = explode(';',$this->val['f']);
+        for($i=0;$i<count($filesDate);$i++) {
+            if(!empty($filesDate[$i])) {
+                if(filemtime($this->temp.'/'.md5($this->val['f']).'.css') < filemtime(PATH.$D_css[$filesDate[$i]])) {
+                    $bundle = false;
+                };
             };
         }
-        if($this->registry()->config['env']=='production') {
-            $this->css = strtr($this->css, $this->colors);
-            $this->css = preg_replace("/[\/*]{2}(.)+[*\/]{2}/sU", '', $this->css);
-            $this->css = strtr($this->css,
-                array(';;'      =>  ';',
-                      "\r"      =>  '',
-                      "\n"      =>  '',
-                      "\t"      =>  '',
-                      ": "      =>  ':',
-                      ' ;'      =>  ';',
-                      '  '      =>  '',
-                      ' {'      =>  '{',
-                      ', '      =>  ',',
-                      ' 0px'    =>  ' 0',
-                      ':0px'    =>  ':0',
-                      ' /'      =>  '',
-                      '/**/'    =>  '',
-                )
-            );
 
-            $this->css = strtr($this->css,
-                array('{path}'   =>  $this->registry()->config['base_href'].$this->registry()->config['app_path'].'/theme/images',
-                      '{library}'=>  $this->registry()->config['base_href'].$this->registry()->config['library_path'])
-            );
 
+        if(is_file($this->temp.'/'.md5($this->val['f']).'.css') && $this->registry()->config['env']=='production' && $bundle) {
             header("Content-Encoding: gzip");
             header("Content-type: text/css", true);
-            header("Cache-Control: max-age=2592000");
+            header("Cache-Control: must-revalidate, max-age=1200");
             header("Pragma: public");
             header("ETag: ".md5($this->val['f']));
+            $this->css = file_get_contents($this->temp.'/'.md5($this->val['f']).'.css');
             echo gzencode($this->css, 9, FORCE_GZIP);
-
-            file_put_contents($this->temp.'/'.md5($this->val['f']).'.css', $this->css);
+            exit();
         } else {
-            header("Content-type: text/css", true);
-            header("Cache-Control: no-store, no-cache, must-revalidate");
-            header("Cache-Control: post-check=0, pre-check=0", false);
-            header("Pragma: no-cache");
+            $files = explode(';',$this->val['f']);
+            for($i=0;$i<count($files);$i++) {
+                if(!empty($files[$i])) {
+                    $this->css .= file_get_contents(PATH.$D_css[$files[$i]]);
+                };
+            }
+            if($this->registry()->config['env']=='production') {
+                $this->css = strtr($this->css, $this->colors);
+                $this->css = preg_replace("/[\/*]{2}(.)+[*\/]{2}/sU", '', $this->css);
+                $this->css = strtr($this->css,
+                    array(';;'      =>  ';',
+                        "\r"      =>  '',
+                        "\n"      =>  '',
+                        "\t"      =>  '',
+                        ": "      =>  ':',
+                        ' ;'      =>  ';',
+                        '  '      =>  '',
+                        ' {'      =>  '{',
+                        ', '      =>  ',',
+                        ' 0px'    =>  ' 0',
+                        ':0px'    =>  ':0',
+                        ' /'      =>  '',
+                        '/**/'    =>  '',
+                    )
+                );
 
-            $this->css = strtr($this->css,
-                array('{path}'   =>  $this->registry()->config['base_href'].$this->registry()->config['app_path'].'/theme/images',
-                      '{library}'=>  $this->registry()->config['base_href'].$this->registry()->config['library_path'])
-            );
-            echo $this->css;
-        };
+                $this->css = strtr($this->css,
+                    array('{path}'   =>  $this->registry()->config['base_href'].$this->registry()->config['app_path'].'/theme/images',
+                        '{library}'=>  $this->registry()->config['base_href'].$this->registry()->config['library_path'])
+                );
+
+                header("Content-Encoding: gzip");
+                header("Content-type: text/css", true);
+                header("Pragma: public");
+                header("ETag: ".md5($this->val['f']));
+                echo gzencode($this->css, 9, FORCE_GZIP);
+
+                file_put_contents($this->temp.'/'.md5($this->val['f']).'.css', $this->css);
+            } else {
+                header("Content-type: text/css", true);
+                header("Cache-Control: no-store, no-cache, must-revalidate");
+                header("Pragma: no-cache");
+
+                $this->css = strtr($this->css,
+                    array('{path}'   =>  $this->registry()->config['base_href'].$this->registry()->config['app_path'].'/theme/images',
+                          '{library}'=>  $this->registry()->config['base_href'].$this->registry()->config['library_path'])
+                );
+                echo $this->css;
+            };
+        }
     }
 }
