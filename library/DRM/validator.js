26d22e34fa874ada68e31ff53755244fb9fc5bce
babak <babak.47031@gmail.com>
2013-01-19 02:04:38 +0200

    validator 1

--- library/DRM/validator.js
+++ library/DRM/validator.js
@@ -8,7 +8,7 @@
                 _number: /-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?/,
                 _dateYYYYmmdd: /\d{4}[\/\-]\d{1,2}[\/\-]\d{1,2}/,
                 _datemmddYYYY: /\d{1,2}[\/\-]\d{1,2}[\/\-]\d{4}/,
-                _login: /^[a-zA-Z0-9_]+$/,
+                _login: /[a-zA-Z0-9_]+/,
                 _password: /[a-zA-Z0-9_-]/,
                 _text: /[\w!@$%^&*()№_+|\-\\\/,=.?'";:а-яА-ЯіІїЇєЄёЁ\s\ ]/,
                 _num: /[0-9]/,
@@ -25,53 +25,66 @@
             },
             classes: {
                 error: '-input-error',
+                warning: '-input-warning',
                 valid: '-valid',
                 active: '-active'
             }
         };
-        var data = $.extend(settings, options);
-        var error = 0;    
+        var data = $.extend(settings, options);   
 
-        $('.'+data.classes.error).live('mouseleave', function() {
-            $(this).next('tooltip').removeClass('show');
-        });
-        $('.'+data.classes.error).live('mouseover', function() {
-            $(this).next('tooltip').addClass('show');
+        $('.'+data.classes.error).live('mouseover mouseout', function() {
+            if(!$(this).prev().is(':focus')) {
+                $(this).next().toggleClass('show');
+            }
         });
 
-
-        var addMessage = function(el, icon, message) {
+        var addMessage = function(el, message) {
             if(!el.next().hasClass(data.classes.error)) {
                 var coords = el.position();
-                var top = coords.top-el.height()-25 +'px';
-                var left = coords.left+el.width()-7 +'px';
+                var top = coords.top-el.height();
+                var left = coords.left+el.width();
 
-                el.after('<span class="tooltip show" style="top: '+top+';left: '+left+'">'+message+'<div class="arrow-down"></div></span>');
-                el.after(icon);
-                error++;
+                el.addClass(data.classes.warning);
+                el.after('<div class="tooltip show">'+message+'<i class="arrow-down"></i></div>');
+                el.after('<i class="'+data.classes.error+'">i</i>');
+                el.next().css({'top':top+18,'left':left-5});
+                el.next().next().css({'top':top-25,'left':left-7});
             };
         };    
 
         var doValidation = function(event) {
             selector.live(event, function() {
                 var el = $(this);
+                var error = 0; 
                 var _class = el.attr('class').split(' ');
-                var icon = '<span class="'+data.classes.error+'">i</span>';
 
                 for(var i=0;i<_class.length;i++) {
-                    if((el.val().length>0 || event=='focusout') && data.regex[_class[i]]) {
-                        addMessage(el, icon, 'test');
+                    if(el.val().length>0 && data.regex[_class[i]]) {
+                        if(!data.regex[_class[i]].test($(this).val())) {
+                            addMessage(el, 'test');
+                            error++;
+                        }
                     };
+
                     if(el.val().length > el.attr('maxlength')) {
-                        addMessage(el, icon, 'test123');
+                        addMessage(el, 'test123');
+                        error++;
                     };
-                    if(el.val().length < el.data('min')) {
-                        addMessage(el, icon, 'test321');
+
+                    if(el.val().length < el.data('min') && el.val().length>0) {
+                        addMessage(el, message.minChar);
+                        error++;
                     };
 
                     if(error===0) {
-                        el.next().remove('span.'+data.classes.error);
+                        el.next().remove('i.'+data.classes.error);
+                        el.next().remove('div.tooltip');
+                        el.removeClass(data.classes.warning);
                     };
+                }
+
+                if(event == 'focusin' || event == 'focusout') {
+                    el.next().next().toggleClass('show');
                 }
             });
         };
